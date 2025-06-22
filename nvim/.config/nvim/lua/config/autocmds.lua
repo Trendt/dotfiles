c53dcad430e1dd4042dfa128a50a1c71be123d4f
trendt <jaroschmidt@web.de>
2025-06-22 19:11:26 +0200

    global custom augroup

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,25 +1,29 @@
+vim.api.nvim_create_augroup("CustomGlobal", { clear = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight on yank",
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ timeout = 150 })
-	end,
+  desc = "Highlight on yank",
+  group = "CustomGlobal",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 150 })
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	desc = "Format on save",
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if not client then return end
+  desc = "Format on save",
+  group = "CustomGlobal",
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
 
-		vim.diagnostic.config({ virtual_text = true })
-		if client.supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				buffer = args.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-				end,
-			})
-		end
-	end
+    vim.diagnostic.config({ virtual_text = true })
+    if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
+  end
 })
