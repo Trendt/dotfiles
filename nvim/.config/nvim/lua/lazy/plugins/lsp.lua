5c7be89ad3dea567f2d9b0c11645eff1022aa6d4
trendt <jaroschmidt@web.de>
2025-06-22 16:27:46 +0200

    basic lsp keybinds, lsp settings not done

--- nvim/.config/nvim/lua/lazy/plugins/lsp.lua
+++ nvim/.config/nvim/lua/lazy/plugins/lsp.lua
@@ -1,38 +1,36 @@
 -- TODO! check
 local lsp_list = {
-	"lua_ls",
-	"tinymist",
-	"rust_analyzer",
-	"clangd",
+  "lua_ls",
+  "tinymist",
+  "rust_analyzer",
+  "clangd",
 }
 
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						path = "${rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-			{ "saghen/blink.cmp" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup()
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            path = "${rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+      { "saghen/blink.cmp" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
 
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			for _, lsp in ipairs(lsp_list) do
-				require("lspconfig")[lsp].setup { capabilities = capabilities }
-			end
-
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-		end
-	},
+      for _, lsp in ipairs(lsp_list) do
+        require("lspconfig")[lsp].setup { capabilities = capabilities }
+      end
+    end
+  },
 }
--- nvim/.config/nvim/lua/lazy/plugins/telescope.lua
+++ nvim/.config/nvim/lua/lazy/plugins/telescope.lua
@@ -1,14 +1,34 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		require("telescope").setup()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fd', builtin.git_files, { desc = 'Telescope find git files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-	end
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require("telescope").setup({})
+    local builtin = require("telescope.builtin")
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fd', builtin.git_files, { desc = 'Telescope find git files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    -- LSP keymaps
+    vim.api.nvim_create_augroup("CustomTelescopeAutocmds", { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = "CustomTelescopeAutocmds",
+      callback = function()
+        local builtin = require("telescope.builtin")
+
+        vim.keymap.set('n', 'grr', builtin.lsp_references, { desc = 'Lsp references' })
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'Lsp goto definiton' })
+        vim.keymap.set('n', 'gtd', builtin.lsp_type_definitions, { desc = 'Lsp goto type definitions' })
+        vim.keymap.set('n', 'gri', builtin.lsp_implementations, { desc = 'Lsp goto implementation' })
+        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { desc = 'Lsp open document symbols of current buffer' })
+        vim.keymap.set('n', 'ggO', builtin.lsp_dynamic_workspace_symbols,
+          { desc = 'Lsp open workspace("global") symbols of current buffer' })
+        -- vim.keymap.set('n', 'gD', builtin.diagnostics,
+        --   { desc = 'open diagnostics' })
+      end,
+    })
+  end
 }
